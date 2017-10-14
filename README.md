417d966222c93a18113c326ce157b2d015f8c02e
hongmk <isentator@naver.com>
2017-10-14 16:11:27 +0900

    restfulExam과 연동구현

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# AngularJSExam
-2017하반기 모바일 교육 (AngularJS, Bootstrap을 이용한 Web App ) 예제
+# angular-jsexam
+
+This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
+version 0.16.0.
+
+## Build & development
+
+Run `grunt` for building and `grunt serve` for preview.
+
+## Testing
+
+Running `grunt test` will run the unit tests with karma.
---
+++ app/scripts/controllers/signup.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJsexamApp.controller:SignupCtrl
+ * @description
+ * # SignupCtrl
+ * Controller of the angularJsexamApp
+ */
+angular.module('angularJsexamApp')
+  .controller('SignupCtrl', [ "Data", "$scope", "$state", 
+  	function (Data, $scope, $state) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.name = "";
+    $scope.age = "";
+
+    $scope.saveUserInfo = function(){
+    	var dataPromise = Data.setData(
+    		'http://127.0.0.1:52273/user',
+    		'&name='+$scope.name+'&age='+$scope.age);
+
+    	dataPromise.then(function(results){
+    		$scope.name = "";
+    		$scope.age = "";
+    	}, function(reason){}, function(update){});
+    };
+
+  }]);
